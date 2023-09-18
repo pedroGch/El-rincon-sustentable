24e614661108f9c2f84eb431c72103d1843b6226
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-18 20:41:39 -0300

    view contacto + ContactoController

---
+++ app/Http/Controllers/ContactoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContactoController extends Controller
+{
+  public function index()
+  {
+    return view('contacto');
+  }
+}
---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,36 @@
+<?PHP
+?>
+
+@extends('layouts.main')
+
+{{-- @section('title') Página Principal @endsection --}}
+
+@section('title', 'Inicio')
+
+@section('content')
+<h1>Contacto</h1>
+
+<form action="#">
+
+  <label for="nombre"> Nombre </label>
+  <input type="text" name="nombre" id="nombre" required>
+
+  <label for="email"> Email </label>
+  <input type="email" name="email" id="email" required>
+
+  <label for="asunto"> Asunto </label>
+  <input type="text" name="asunto" id="asunto" required>
+
+  <label for="mensaje"> Mensaje </label>
+  <textarea name="mensaje" id="mensaje" cols="30" rows="10" required></textarea>
+
+  <input type="submit" value="Enviar">
+
+
+</form>
+
+
+
+
+
+@endsection
