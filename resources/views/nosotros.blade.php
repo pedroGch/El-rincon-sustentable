e10aac46e5b9c328c66b272694b2c7787660732a
Ezequiel <ezequiel.valdiviezo@davinci.edu.ar>
2023-09-18 20:24:52 -0300

    Creacion de archivos nosotros

---
+++ resources/views/nosotros.blade.php
@@ -0,0 +1,12 @@
+<?PHP
+?>
+
+@extends('layouts.main')
+
+{{-- @section('title') Página Principal @endsection --}}
+
+@section('title', 'Nosotros')
+
+@section('content')
+<h1>Nosotros</h1>
+@endsection
