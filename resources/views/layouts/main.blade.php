ce63ba436485ec10dac4ac419b55583772106e9a
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-09-18 14:26:02 -0300

    agrego al proyecto tailwind

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=], initial-scale=1.0">
-    <link rel="stylesheet" href=" <?= url('css/styles.css') ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/styles.css')
 
     <title> @yield('title') :: El Rincón Sustentable</title>
 </head>
@@ -21,7 +21,7 @@
             @yield('content')
         </main>
         <footer>
-            <p>Portales y Comercio Electrónico - Parcial 1</p>
+            <p class="text-3xl font-bold underline">Portales y Comercio Electrónico - Parcial 1</p>
             <p>Gonzalez Chavez Pedro - Scotto Rocío - Valdiviezo Ezequiel</p>
         </footer>
     </div>
