6d64dd26d41da5e4303a142c1138ab96ef739a8f
Ezequiel <ezequiel.valdiviezo@davinci.edu.ar>
2023-09-18 23:12:19 -0300

    Estructura html base de nosotros

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -9,4 +9,60 @@
 
 @section('content')
 <h1>Nosotros</h1>
+
+<div>
+  <div>
+    <img src="" alt="">
+  </div>
+  <div>
+      <p>Desde 1980 nos enorgullece ser una tienda comprometida con la promoción de prácticas responsables y sostenibles que ayuden a preservar nuestro planeta.</p>
+      <p>Comenzamos siendo una empresa familiar y actualmente contamos con un equipo de 50 personas.</p>
+      <p><strong>En El Rincón Sustentable, nos enfocamos en ofrecer productos diseñados especialmente para reducir la huella de carbono y minimizar el impacto negativo
+        en el medio ambiente.</strong>Cada uno de nuestros artículos es cuidadosamente seleccionado y evaluado para garantizar que cumpla con nuestros estándares de alta calidad, eficiencia y amigabilidad con el medio ambiente.</p>
+  </div>
+</div>
+
+<div class="bg-green-700">
+  <p>Nuestro objetivo es brindarte soluciones sostenibles para tus necesidades diarias, sin comprometer el confort ni la calidad. Creemos firmemente que cada pequeña
+    acción cuenta y que juntos podemos marcar la diferencia en la protección de nuestro entorno.</p>
+</div>
+
+<div>
+  <div>
+    <div>
+      <img src="" alt="">
+    </div>
+    <div>
+      <p>Ernesto Arriaga</p>
+      <p>Presidente</p>
+    </div>
+  </div>
+  <div>
+    <div>
+      <img src="" alt="">
+    </div>
+    <div>
+      <p>Micaela Arriaga</p>
+      <p>Responsable de Ventas</p>
+    </div>
+  </div>
+  <div>
+    <div>
+      <img src="" alt="">
+    </div>
+    <div>
+      <p>Federico Risso</p>
+      <p>Diseño de Producto</p>
+    </div>
+  </div>
+  <div>
+    <div>
+      <img src="" alt="">
+    </div>
+    <div>
+      <p>Andres Barcala</p>
+      <p>Lic. Seguridad e Higiene</p>
+    </div>
+  </div>
+</div>
 @endsection
