ff1b964bc5b0156dd17432398abc6a0b8bf1d434
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-19 19:03:14 -0300

    alineación h2 welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,9 @@
 @section('title', 'Inicio')
 
 @section('content')
-<h2 class="text-principal my-4 mt-10 text-4xl font-semibold">Bienvenidos al Rincón Sustentable</h2>
+
+<div class="flex justify-center">
+  <h2 class="text-principal my-4 mt-10 text-4xl font-semibold">Bienvenidos al Rincón Sustentable</h2>
+</div>
 
 @endsection
