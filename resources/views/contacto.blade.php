4a73be0ea4835980173fe8b76bcb690ce298e5a3
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-18 23:09:40 -0300

    Form contacto

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -9,26 +9,32 @@
 @section('title', 'Contacto')
 
 @section('content')
-<h1>Contacto</h1>
+<div class="container">
 
-<form action="#">
-
-  <label for="nombre"> Nombre </label>
-  <input type="text" name="nombre" id="nombre" required>
-
-  <label for="email"> Email </label>
-  <input type="email" name="email" id="email" required>
-
-  <label for="asunto"> Asunto </label>
-  <input type="text" name="asunto" id="asunto" required>
-
-  <label for="mensaje"> Mensaje </label>
-  <textarea name="mensaje" id="mensaje" cols="30" rows="10" required></textarea>
-
-  <input type="submit" value="Enviar">
+  <h1>Contacto</h1>
 
 
-</form>
+  <div class="row">
+
+    <div class="columns-1">
+      <img src="./../../../public/img/envelope.jpg " alt="">
+    </div>
+
+    <div class="columns-1">
+      <form action="#" method="POST" id="form-contacto" class="">
+        <label for="nombre" class="block font-bold mb-1"> Nombre </label>
+        <input type="text" name="nombre" id="nombre" class="border border-gray-500 rounded p-2 w-full" required>
+        <label for="email" class="block font-bold mb-1"> Email </label>
+        <input type="email" name="email" id="email" class="border border-gray-500 rounded p-2 w-full" required>
+        <label for="asunto" class="block font-bold mb-1"> Asunto </label>
+        <input type="text" name="asunto" id="asunto" class="border border-gray-500 rounded p-2 w-full" required>
+        <label for="mensaje" class="block font-bold mb-1"> Mensaje </label>
+        <textarea name="mensaje" id="mensaje" cols="30" rows="10" class="border border-gray-500 rounded p-2 w-full" required></textarea>
+        <input type="submit" value="Enviar">
+      </form>
+    </div>
+  </div>
+</div>
 
 
 
