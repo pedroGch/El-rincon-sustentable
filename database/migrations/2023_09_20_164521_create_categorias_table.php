7672ce8358c056bd0616dcdaa172949854ffa389
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-20 13:53:44 -0300

    creación de migrations => noticias & categorias

---
+++ database/migrations/2023_09_20_164521_create_categorias_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categorias', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_cat', 100);
+            $table->string('descripcion', 255);
+            // created_at, updated_at
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categorias');
+    }
+};
