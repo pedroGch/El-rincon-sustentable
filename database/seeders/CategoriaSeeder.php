42590efd636dca86b8ae99667203214aa62df0e6
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-09-20 14:36:45 -0300

    corrección nombre tabla CategoriaSeeder

--- database/seeders/CategoriaSeeder.php
+++ database/seeders/CategoriaSeeder.php
@@ -13,27 +13,27 @@ class CategoriaSeeder extends Seeder
      */
     public function run(): void
     {
-      DB::table('noticias')->insert(
+      DB::table('categorias')->insert(
         [
           [
             'id' => 1,
             'nombre_cat' => 'Paneles Solares',
-            'descripcion' => '',
+            'descripcion' => 'Obtené energía limpia y sostenible con nuestros paneles solares de alta eficiencia. Convertí la luz del sol en electricidad para tu hogar de manera eco-amigable y ahorrá en tus facturas de energía.',
           ],
           [
             'id' => 2,
             'nombre_cat' => 'Composteras',
-            'descripcion' => '',
+            'descripcion' => 'Transformá tus residuos orgánicos en valioso compost con nuestras composteras. Fomentá la sostenibilidad y nutre tu jardín con un ciclo de vida natural y respetuoso con el medio ambiente.',
           ],
           [
             'id' => 3,
             'nombre_cat' => 'Cestos de Basura',
-            'descripcion' => '',
+            'descripcion' => 'Mantené tu entorno limpio y organizado con nuestros cestos de basura de alta calidad. Diseñados para la eficiencia en la recolección de residuos, hacen que la gestión de desechos sea más simple y efectiva.',
           ],
           [
             'id' => 4,
             'nombre_cat' => 'Yo Reutilizo!',
-            'descripcion' => '',
+            'descripcion' => 'Sumate al movimiento "Yo Reutilizo!" y reducí tu huella ecológica. Descubrí productos reutilizables y soluciones eco-amigables que te ayudarán a cuidar el planeta mientras ahorrás dinero a largo plazo.',
           ]
         ]
       );
