2db0d3987caf31ef54e355da1a3c6837e47ad885
Ezequiel <ezequiel.valdiviezo@davinci.edu.ar>
2023-09-21 19:41:14 -0300

    Cree migration + model de productos

---
+++ database/migrations/2023_09_21_222725_create_productos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_prod', 256);
+            $table->string('imagen_prod', 256);
+            $table->string('alt', 256);
+            $table->unsignedInteger('categoria'); //FK
+            $table->text('descripcion')->nullable();
+            $table->unsignedInteger('stock');
+            $table->unsignedInteger('precio');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
