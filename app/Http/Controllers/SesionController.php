638207935402d0c1ee6b04e0fbb147bbf4311451
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-09-24 16:27:08 -0300

    creo controlador para sesiones

---
+++ app/Http/Controllers/SesionController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class SesionController extends Controller
+{
+  public function iniciar_sesion()
+  {
+    return view('iniciar_sesion');
+  }
+  public function validar_usuario(Request $request)
+  {
+    $usuarios = Usuario::all();
+    dd($usuarios);
+    return view('welcome');
+  }
+  public function cerrar_sesion()
+  {
+    return view('welcome');
+  }
+  public function crear_cuenta()
+  {
+    return view('welcome');
+  }
+}
