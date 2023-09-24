d9f68e5fed51451a5beef57b3d1a371c68008e23
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-09-24 16:26:55 -0300

    agrego rutas para sesion

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -24,6 +24,8 @@
                 <li class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out"><a href="<?= url('/tienda') ?>">Tienda</a></li>
                 <li class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out"><a href="<?= url('/blog') ?>">Blog</a></li>
                 <li class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out"><a href="<?= url('/contacto') ?>">Contacto</a></li>
+                <li class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out"><a href="<?= url('/crear_cuenta') ?>">crear una cuenta</a></li>
+                <li class="mx-3 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out"><a href="<?= url('/iniciar_sesion') ?>">inciar sesión</a></li>
             </ul>
         </nav>
         <main class="container mx-auto flex-grow">
