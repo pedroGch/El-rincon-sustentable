509a15d90c954e5af1b2b90bba4dc3525793d91a
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-09-24 16:27:32 -0300

    modifico modelo de usuario

--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -3,18 +3,51 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-/**
- * @property integer $id
- * @property string $nombre_usuario
- * @property string $rol
- * @property string $password
- */
-class Usuario extends Model
+
+class Usuario extends Authenticatable
 {
   //use HasFactory;
+  use HasApiTokens, HasFactory, Notifiable;
   protected  $table = 'usuarios';
-  protected  $pk = 'id';
+
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+      'nombre_usuario',
+      'rol',
+      'password',
+  ];
+
+  /**
+   * The attributes that should be hidden for serialization.
+   *
+   * @var array<int, string>
+   */
+  protected $hidden = [
+      'password',
+      'remember_token',
+  ];
+
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+      'email_verified_at' => 'datetime',
+      'password' => 'hashed',
+  ];
+
+  public function setPasswordAttribute($value)
+  {
+    $this->attributes['password'] = bcrypt($value);
+  }
 
 }
