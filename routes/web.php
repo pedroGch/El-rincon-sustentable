759be0efaaa26269104563e246b9ef83e7674b76
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-09-29 20:40:17 -0300

    habilito ruta de para alta de noticia

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,6 @@ Route::get('/blog', [\App\Http\Controllers\BlogController::class, 'index']);
 Route::get('/blog/{id}/leer_mas', [\App\Http\Controllers\BlogController::class, 'leerCompleto']);
 Route::get('/blog/gestor_noticias', [\App\Http\Controllers\BlogController::class, 'obtenerNoticias']);
 Route::get('/blog/noticia/nueva', [\App\Http\Controllers\BlogController::class, 'formularioCrearNoticia']);
-//Route::post('/blog/noticia/nueva', [\App\Http\Controllers\BlogController::class, 'crearNoticia']);
+Route::post('/blog/noticia/nueva', [\App\Http\Controllers\BlogController::class, 'crearNoticia']);
 Route::get('/blog/{id}/editar', [\App\Http\Controllers\BlogController::class, 'editarNoticia']);
 Route::post('/blog/{id}/eliminar', [\App\Http\Controllers\BlogController::class, 'borrarNoticia']);
