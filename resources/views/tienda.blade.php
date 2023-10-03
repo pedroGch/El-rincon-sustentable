ae9d18aacb48f83046557d5f266ca79482430994
Ezequiel <ezequiel.valdiviezo@davinci.edu.ar>
2023-10-02 21:41:41 -0300

    Alinear las card de tienda

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -14,10 +14,11 @@
           @foreach ($productos as $producto)
             <div class="flex flex-col w-full lg:w-1/4 md:w-1/2 p-4">
               <div class="block rounded-lg bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-neutral-700">
-                  <div class="relative overflow-hidden bg-cover bg-no-repeat" data-te-ripple-init
+                  <div class="relative overflow-hidden bg-cover bg-no-repeat mx-auto" data-te-ripple-init
                       data-te-ripple-color="light">
-                      <img class="rounded-t-lg" src="{{ asset("img/productos/{$producto->imagen_prod}") }}" alt="<?=$producto->alt?>" />
-                      <a href="#!">
+                      <div class="h-72  mx-auto">
+                      <img class="rounded-t-lg mx-auto" src="{{ asset("img/productos/{$producto->imagen_prod}") }}" alt="<?=$producto->alt?>" />
+                      <a href="#!"></div>
                           <div
                               class="absolute bottom-0 left-0 right-0 top-0 h-full w-full overflow-hidden bg-[hsla(0,0%,98%,0.15)] bg-fixed opacity-0 transition duration-300 ease-in-out hover:opacity-100">
                           </div>
