7d7950e3645085851d0419b76e129a3ec20a5fbf
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-07 18:07:17 -0300

    estilos validacion crear noticia form

--- resources/views/blog/formulario_edit_noticia.blade.php
+++ resources/views/blog/formulario_edit_noticia.blade.php
@@ -2,7 +2,7 @@
 /**
  * @var \Illuminate\Support\ViewErrorBag $errors
  * @var Noticia $noticia
-*/
+ */
 ?>
 @extends('layouts.main')
 
@@ -11,70 +11,72 @@
 @section('title', 'Editar Noticia' . $noticia->titulo)
 
 @section('content')
-@if ($errors->has('title'))
-  <p class="text-red-400">{{$errors->first('titulo')}}</p>
-@endif
-<section class="mx-auto max-w-screen-xl">
-  <div class="flex justify-center">
-    <h2 class="text-principal my-4 mt-10 text-4xl font-semibold">Editar Noticia</h2>
-  </div>
-  <div class="flex-1 my-4 mx-6 px-6">
-    <form action="{{url('/blog/'.$noticia->id.'/editar')}}" method="POST" id="form-contacto" class="">
-      @csrf
-      <label for="titulo" class="block font-bold my-2"> Título </label>
-      <input type="text" value="{{old('titulo', $noticia->titulo)}}" name="titulo" id="titulo" class="border border-gray-500 rounded p-2 w-full @error('titulo') border-red-500 @enderror"
-      @error('titulo')
+    @if ($errors->has('title'))
+        <p class="text-red-400">{{ $errors->first('titulo') }}</p>
+    @endif
+    <section class="mx-auto max-w-screen-xl">
+        <div class="flex justify-center">
+            <h2 class="text-principal my-4 mt-10 text-4xl font-semibold">Editar Noticia</h2>
+        </div>
+        <div class="flex-1 my-4 mx-6 px-6">
+            <form action="{{ url('/blog/' . $noticia->id . '/editar') }}" method="POST" id="form-contacto" class="">
+                @csrf
+                <label for="titulo" class="block font-bold my-2"> Título </label>
+                <input type="text" value="{{ old('titulo', $noticia->titulo) }}" name="titulo" id="titulo"
+                    class="border border-gray-500 rounded p-2 w-full @error('titulo') border-red-500 @enderror"
+                    @error('titulo')
         aria-invalid="true"
         aria-describedby="error-titulo"
-      @enderror
-      >
-      @error('titulo')
-        <p class="text-red-400" id="error-titulo">ERROR! {{$message}}</p>
-      @enderror
+      @enderror>
+                @error('titulo')
+                    <p class="text-red-400" id="error-titulo">ERROR! {{ $message }}</p>
+                @enderror
 
-      <label for="contenido" class="block font-bold my-2"> Contenido </label>
-      <textarea name="contenido" id="contenido" cols="30" rows="10" class="border border-gray-500 rounded p-2 w-full @error('contenido') border-red-500 @enderror"
-      @error('contenido')
+                <label for="contenido" class="block font-bold my-2"> Contenido </label>
+                <textarea name="contenido" id="contenido" cols="30" rows="10"
+                    class="border border-gray-500 rounded p-2 w-full @error('contenido') border-red-500 @enderror"
+                    @error('contenido')
         aria-invalid="true"
         aria-describedby="error-contenido"
-      @enderror
-      >
-      {{old('titulo', $noticia->contenido)}}
+      @enderror>
+      {{ old('titulo', $noticia->contenido) }}
       </textarea>
-      @error('contenido')
-        <p class="text-red-400" id="error-contenido">ERROR! {{$message}}</p>
-      @enderror
+                @error('contenido')
+                    <p class="text-red-400" id="error-contenido">ERROR! {{ $message }}</p>
+                @enderror
 
-      <label for="imagen" class="block font-bold my-2"> Imagen </label>
-      <input type="file" name="imagen" id="imagen" class="border border-gray-500 rounded p-2 w-full @error('imagen') border-red-500 @enderror"
-      @error('imagen')
+                <label for="imagen" class="block font-bold my-2"> Imagen </label>
+                <input type="file" name="imagen" id="imagen"
+                    class="border border-gray-500 rounded p-2 w-full @error('imagen') border-red-500 @enderror"
+                    @error('imagen')
         aria-invalid="true"
         aria-describedby="error-imagen"
-      @enderror
-      >
-      @error('imagen')
-        <p class="text-red-400" id="error-imagen">ERROR! {{$message}}</p>
-      @enderror
+      @enderror>
+                @error('imagen')
+                    <p class="text-red-400" id="error-imagen">ERROR! {{ $message }}</p>
+                @enderror
 
-      <label for="alt" class="block font-bold my-2"> Descripción de la imagen</label>
-      <input type="text" name="alt" id="alt" class="border border-gray-500 rounded p-2 w-full @error('alt') border-red-500 @enderror"
-      @error('alt')
+                <label for="alt" class="block font-bold my-2"> Descripción de la imagen</label>
+                <input type="text" name="alt" id="alt"
+                    class="border border-gray-500 rounded p-2 w-full @error('alt') border-red-500 @enderror"
+                    @error('alt')
         aria-invalid="true"
         aria-describedby="error-alt"
-      @enderror
-      >
-      @error('alt')
-        <p class="text-red-400" id="error-alt">ERROR! {{$message}}</p>
-      @enderror
+      @enderror>
+                @error('alt')
+                    <p class="text-red-400" id="error-alt">ERROR! {{ $message }}</p>
+                @enderror
 
-      <input type="submit" class="my-7 ps-9 pe-9 inline-block rounded bg-terciario px-6 pb-2 pt-2.5 text-s font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-terciario hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-terciario focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-terciario active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]" value="Enviar">
-    </form>
-    <a href="<?= url('/blog/gestor_noticias') ?>">
-      <div
-          class="inline-block rounded bg-terciario px-6 pb-2 pt-2.5 text-s font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-terciario hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-terciario focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-terciario active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
-          <p>Cancelar</p>
-      </div>
-  </a>
-  </div>
-</section>
+                <input type="submit"
+                    class="my-7 ps-9 pe-9 inline-block rounded bg-terciario px-6 pb-2 pt-2.5 text-s font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-terciario hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-terciario focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-terciario active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    value="Enviar">
+            </form>
+            <a href="<?= url('/blog/gestor_noticias') ?>">
+                <div
+                    class="inline-block rounded bg-terciario px-6 pb-2 pt-2.5 text-s font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-terciario hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-terciario focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-terciario active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]">
+                    <p>Cancelar</p>
+                </div>
+            </a>
+        </div>
+    </section>
 @endsection
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,90 +15,89 @@
     <div id="app" class="min-h-screen flex flex-col">
         <nav class="w-screen h-[85px] bg-principal drop-shadow-xl flex items-center justify-between">
             <div class="flex items-center">
-              <div class="mx-6 bg-white w-[70px] h-[85px]  shadow-lg">
-                  <img src="{{ asset('img/logo.png') }}" alt="logo del rincon sustentable" class=" w-screen my-2  ">
-                  <h1 class="text-[0px]">El Rincón Sustentable</h1>
-              </div>
-              <ul class="text-white flex flex-row">
-                  <li
-                      class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
-                      <a href="<?= url('/') ?>">Inicio</a></li>
-                  <li
-                      class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
-                      <a href="<?= url('/nosotros') ?>">Nosotros</a></li>
-                  <li
-                      class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
-                      <a href="<?= url('/tienda') ?>">Tienda</a></li>
-                  <li
-                      class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
-                      <a href="<?= url('/blog') ?>">Blog</a></li>
-                  <li
-                      class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
-                      <a href="<?= url('/contacto') ?>">Contacto</a></li>
-                  <li
-                      class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
-                      <a href="<?= url('/crear_cuenta') ?>">Crear cuenta</a></li>
-                  <li
-                      class="mx-3 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
-                      <a href="<?= url('/iniciar_sesion') ?>">Iniciar sesión</a></li>
-              </ul>
+                <div class="mx-6 bg-white w-[70px] h-[85px]  shadow-lg">
+                    <img src="{{ asset('img/logo.png') }}" alt="logo del rincon sustentable" class=" w-screen my-2  ">
+                    <h1 class="text-[0px]">El Rincón Sustentable</h1>
+                </div>
+                <ul class="text-white flex flex-row">
+                    <li
+                        class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
+                        <a href="<?= url('/') ?>">Inicio</a>
+                    </li>
+                    <li
+                        class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
+                        <a href="<?= url('/nosotros') ?>">Nosotros</a>
+                    </li>
+                    <li
+                        class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
+                        <a href="<?= url('/tienda') ?>">Tienda</a>
+                    </li>
+                    <li
+                        class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
+                        <a href="<?= url('/blog') ?>">Blog</a>
+                    </li>
+                    <li
+                        class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
+                        <a href="<?= url('/contacto') ?>">Contacto</a>
+                    </li>
+                    <li
+                        class="mx-6 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
+                        <a href="<?= url('/crear_cuenta') ?>">Crear cuenta</a>
+                    </li>
+                    <li
+                        class="mx-3 h-[30px] text-lg hover:border-b-[5px] hover:border-yellow-500 transition duration-300 ease-in-out">
+                        <a href="<?= url('/iniciar_sesion') ?>">Iniciar sesión</a>
+                    </li>
+                </ul>
             </div>
 
             <div class="relative me-10" data-te-dropdown-ref>
-              <button
-                class="flex items-center whitespace-nowrap rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out  hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]  focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
-                type="button"
-                id="dropdownMenuButton1d"
-                data-te-dropdown-toggle-ref
-                aria-expanded="false"
-                data-te-ripple-init
-                data-te-ripple-color="light">
-                <img src="{{  url('./../public/img/avatar/avatar-valdi.jpg')}}" class="w-[40px] rounded-full shadow-lg"
-                alt="Avatar" />
-                <span class="ml-2 w-2">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    class="h-5 w-5">
-                    <path
-                      fill-rule="evenodd"
-                      d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
-                      clip-rule="evenodd" />
-                  </svg>
-                </span>
-              </button>
-              <ul
-                class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
-                aria-labelledby="dropdownMenuButton1d"
-                data-te-dropdown-menu-ref>
-                <li>
-                  <a
-                    class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                    href="#"
-                    data-te-dropdown-item-ref
-                    >Mi Perfil</a
-                  >
-                </li>
+                <button
+                    class="flex items-center whitespace-nowrap rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out  hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)]  focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] motion-reduce:transition-none dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+                    type="button" id="dropdownMenuButton1d" data-te-dropdown-toggle-ref aria-expanded="false"
+                    data-te-ripple-init data-te-ripple-color="light">
+                    <img src="{{ url('./../public/img/avatar/avatar-valdi.jpg') }}"
+                        class="w-[40px] rounded-full shadow-lg" alt="Avatar" />
+                    <span class="ml-2 w-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="h-5 w-5">
+                            <path fill-rule="evenodd"
+                                d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                </button>
+                <ul class="absolute z-[1000] float-left m-0 hidden min-w-max list-none overflow-hidden rounded-lg border-none bg-white bg-clip-padding text-left text-base shadow-lg dark:bg-neutral-700 [&[data-te-dropdown-show]]:block"
+                    aria-labelledby="dropdownMenuButton1d" data-te-dropdown-menu-ref>
+                    <li>
+                        <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                            href="#" data-te-dropdown-item-ref>Mi Perfil</a>
+                    </li>
 
 
-                <hr
-                  class="my-2 h-0 border border-t-0 border-solid border-neutral-700 opacity-25 dark:border-neutral-200" />
-                <li>
-                  <a
-                    class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                    href="#"
-                    data-te-dropdown-item-ref
-                    >Cerrar sesión</a
-                  >
-                </li>
-              </ul>
+                    <hr
+                        class="my-2 h-0 border border-t-0 border-solid border-neutral-700 opacity-25 dark:border-neutral-200" />
+                    <li>
+                        <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
+                            href="#" data-te-dropdown-item-ref>Cerrar sesión</a>
+                    </li>
+                </ul>
             </div>
         </nav>
         <main class="container mx-auto flex-grow">
             @if (\Session::has('status.message'))
-                <div>
-                    {{ \Session::get('status.message') }}
+                
+                <div class="mt-5 mb-3 inline-flex w-full items-center rounded-lg bg-success-100 px-6 py-5 text-base text-success-700"
+                    role="alert">
+                    <span class="mr-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="h-5 w-5">
+                            <path fill-rule="evenodd"
+                                d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+                                clip-rule="evenodd" />
+                        </svg>
+                    </span>
+                    <div>
+                      {{ \Session::get('status.message') }}
+                  </div>
                 </div>
             @endif
             <!-- espacio cedido a templates anexos -->
@@ -120,7 +119,7 @@
                         <h3 class="block mb-2 text-2xl font-bold ">Comisión - Cuatrimestre - Año:</h3>
                         <p class="text-xl">DWN4BV - 4to - 2023</p>
                     </div>
-                    <div class="mb-4"><img src="{{  url('./../public/img/logo-davinci.png')}}" class=""
+                    <div class="mb-4"><img src="{{ url('./../public/img/logo-davinci.png') }}" class=""
                             alt="logo Escuela Da Vinci" /></div>
                 </div>
 
@@ -131,8 +130,8 @@
                     <div class="flex">
 
                         <div id="avatar-pedro" class="text-center px-6 pt-8">
-                            <img src="{{  url('./../public/img/avatar/avatar-pedro.jpg')}}" class="mx-auto mb-4 w-32 rounded-lg"
-                                alt="Avatar Pedro Gonzalez Chavez" />
+                            <img src="{{ url('./../public/img/avatar/avatar-pedro.jpg') }}"
+                                class="mx-auto mb-4 w-32 rounded-lg" alt="Avatar Pedro Gonzalez Chavez" />
                             <p class="mb-2 text-xl font-medium leading-tight">Pedro <br>Gonzalez Chavez</p>
                             <div>
                                 <a href="https://github.com/pedroGch" target="_blank">
@@ -149,8 +148,8 @@
                         </div>
 
                         <div id="avatar-rocio" class="text-center px-6 pt-8">
-                            <img src="{{  url('./../public/img/avatar/avatar-rocio.jpg')}}" class="mx-auto mb-4 w-32 rounded-lg"
-                                alt="Avatar Rocío Scotto" />
+                            <img src="{{ url('./../public/img/avatar/avatar-rocio.jpg') }}"
+                                class="mx-auto mb-4 w-32 rounded-lg" alt="Avatar Rocío Scotto" />
                             <p class="mb-2 text-xl font-medium leading-tight">Rocío <br>Scotto</p>
                             <div>
                                 <a href="https://github.com/roscotto" target="_blank">
@@ -167,8 +166,8 @@
                         </div>
 
                         <div id="avatar-valdi" class="text-center px-6 pt-8">
-                            <img src="{{  url('./../public/img/avatar/avatar-valdi.jpg')}}" class="mx-auto mb-4 w-32 rounded-lg"
-                                alt="Avatar Ezequiel Valdiviezo" />
+                            <img src="{{ url('./../public/img/avatar/avatar-valdi.jpg') }}"
+                                class="mx-auto mb-4 w-32 rounded-lg" alt="Avatar Ezequiel Valdiviezo" />
                             <p class="mb-2 text-xl font-medium leading-tight">Ezequiel <br>Valdiviezo</p>
                             <div>
                                 <a href="https://github.com/Valdiviezo-Ezequiel" target="_blank">
