97417915c5352a927fdc837a66b3356134b59696
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-12 18:02:03 -0300

    uso route en vez de url

--- resources/views/tienda.blade.php
+++ resources/views/tienda.blade.php
@@ -48,7 +48,7 @@
                                 class="w-full border-b-2 border-neutral-100 border-opacity-100 px-4 py-3 dark:border-opacity-50">
                                 <strong> {{ $producto->precio_formateado($producto->precio)}}</strong>
                             </p>
-                            <form action="{{ url('/producto/' . $producto->id) }}" method="GET">
+                            <form action="{{ route('detalle.producto',['id' => $producto->id]) }}" method="GET">
                                 <button type="submit"
                                     class="w-full inline-block rounded bg-terciario px-6 pb-2 pt-2.5 text-s font-bold uppercase leading-normal text-black shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-terciario hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-terciario focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-terciario active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
                                     data-te-ripple-init data-te-ripple-color="light">
