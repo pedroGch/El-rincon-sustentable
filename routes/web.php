face34c53ad30f0169e29ada6c8801e30952e3e4
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-12 18:02:41 -0300

    arreglo rutas

--- routes/web.php
+++ routes/web.php
@@ -13,29 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
-Route::get('/nosotros', [\App\Http\Controllers\AboutController::class, 'index']);
-Route::get('/contacto', [\App\Http\Controllers\ContactoController::class, 'index']);
-Route::get('/tienda', [\App\Http\Controllers\ProductoController::class, 'index']);
-Route::get('/producto/{id}', [\App\Http\Controllers\ProductoController::class, 'detalleProducto'])->whereNumber('id');
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index'])
+  ->name('inicio');
+Route::get('/nosotros', [\App\Http\Controllers\AboutController::class, 'index'])
+  ->name('nosotros');
+Route::get('/contacto', [\App\Http\Controllers\ContactoController::class, 'index'])
+  ->name('contacto');
+Route::get('/tienda', [\App\Http\Controllers\ProductoController::class, 'index'])
+  ->name('tienda');
+Route::get('/producto/{id}', [\App\Http\Controllers\ProductoController::class, 'detalleProducto'])
+  ->whereNumber('id')
+  ->name('detalle.producto');
+Route::post('/contacto', [\App\Http\Controllers\ContactoController::class, 'validar_contacto']);
+
+//RUTAS RELACIONADAS AL INICIO DE SESION
 Route::get('/iniciar_sesion', [\App\Http\Controllers\SesionController::class, 'iniciar_sesion']);
 Route::get('/crear_cuenta', [\App\Http\Controllers\SesionController::class, 'crear_cuenta']);
 Route::post('/validar_usuario', [\App\Http\Controllers\SesionController::class, 'validar_usuario']);
-Route::post('/contacto', [\App\Http\Controllers\ContactoController::class, 'validar_contacto']);
+Route::post('/cerrar_sesion', [\App\Http\Controllers\SesionController::class, 'cerrar_sesion']);
 
 //RUTAS RELACIONADAS AL BLOG DE NOTICIAS
 Route::get('/blog', [\App\Http\Controllers\BlogController::class, 'index']);
 Route::get('/blog/{id}/leer_mas', [\App\Http\Controllers\BlogController::class, 'leerCompleto']);
-Route::get('/blog/gestor_noticias', [\App\Http\Controllers\BlogController::class, 'obtenerNoticias']);
-Route::get('/blog/noticia/nueva', [\App\Http\Controllers\BlogController::class, 'formularioCrearNoticia']);
-Route::post('/blog/noticia/nueva', [\App\Http\Controllers\BlogController::class, 'crearNoticia']);
-Route::get('/blog/{id}/editar', [\App\Http\Controllers\BlogController::class, 'formularioEditarNoticia']);
-Route::post('/blog/{id}/editar', [\App\Http\Controllers\BlogController::class, 'editarNoticia']);
+Route::get('/blog/gestor_noticias', [\App\Http\Controllers\BlogController::class, 'obtenerNoticias'])->middleware(['auth']);
+Route::get('/blog/noticia/nueva', [\App\Http\Controllers\BlogController::class, 'formularioCrearNoticia'])->middleware(['auth']);
+Route::post('/blog/noticia/nueva', [\App\Http\Controllers\BlogController::class, 'crearNoticia'])->middleware(['auth']);
+Route::get('/blog/{id}/editar', [\App\Http\Controllers\BlogController::class, 'formularioEditarNoticia'])->middleware(['auth']);
+Route::post('/blog/{id}/editar', [\App\Http\Controllers\BlogController::class, 'editarNoticia'])->middleware(['auth']);
 
 //RUTAS RELACIONADAS AL PANEL DE ADMINISTRACIÓN
-Route::get('/panel_admin', [\App\Http\Controllers\SesionController::class, 'dashboard_admin']);
-Route::get('/panel_admin', [\App\Http\Controllers\BlogController::class, 'accederDesdePanelAdmin']);
-Route::get('/perfil_usuario', [\App\Http\Controllers\SesionController::class, 'perfil_usuario']);
-
-//Route::get('/blog/{id}/eliminar', [\App\Http\Controllers\BlogController::class, 'borrarNoticia'])->whereNumber('id');
+Route::get('/panel_admin', [\App\Http\Controllers\BlogController::class, 'dashboardAdmin'])->middleware(['auth']);
+Route::get('/perfil_usuario', [\App\Http\Controllers\SesionController::class, 'perfil_usuario'])->middleware(['auth']);
 Route::get('/blog/{id}/eliminar', [\App\Http\Controllers\BlogController::class, 'borrarNoticia'])->whereNumber('id');
