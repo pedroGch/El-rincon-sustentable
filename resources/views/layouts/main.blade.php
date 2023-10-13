eefc83ec3e4f231f118d286a9c36963776ad36a1
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-13 20:31:17 -0300

    link a mi perfil desde dropdown

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -73,7 +73,7 @@
                     aria-labelledby="dropdownMenuButton1d" data-te-dropdown-menu-ref>
                     <li>
                         <a class="block w-full whitespace-nowrap bg-transparent px-4 py-2 text-sm font-normal text-neutral-700 hover:bg-neutral-100 active:text-neutral-800 active:no-underline disabled:pointer-events-none disabled:bg-transparent disabled:text-neutral-400 dark:text-neutral-200 dark:hover:bg-neutral-600"
-                            href="#" data-te-dropdown-item-ref>Mi Perfil</a>
+                            href="<?= url('/panel_admin'); ?>" data-te-dropdown-item-ref>Mi Perfil</a>
                     </li>
 
 
