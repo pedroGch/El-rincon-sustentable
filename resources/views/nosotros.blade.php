1a1dea4f419c1d8fd90d5d3c2724d3dd493395cf
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-10-14 11:56:28 -0300

    fix responsive vista nosotros

--- resources/views/nosotros.blade.php
+++ resources/views/nosotros.blade.php
@@ -10,10 +10,10 @@
             <h2 class="text-principal my-4 mt-10 text-4xl font-semibold">Nosotros</h2>
         </div>
         <div class="flex flex-wrap">
-            <div class="flex-1 p-10">
+            <div class="py-3 lg:flex-1 lg:p-10">
                 <img src="../public/img/nosotros.webp" alt="">
             </div>
-            <div class="flex-1 p-10 text-xl m-auto">
+            <div class="lg:flex-1 lg:p-10 lg:text-xl m-auto">
                 <p>Desde 1980 nos enorgullece ser una tienda comprometida con la promoción de prácticas responsables y
                     sostenibles que ayuden a preservar nuestro planeta.</p>
                 <p>Comenzamos siendo una empresa familiar y actualmente contamos con un equipo de 50 personas.</p>
@@ -25,44 +25,44 @@
             </div>
         </div>
 
-        <div class="bg-principal p-16 mb-20 mt-20">
+        <div class="bg-principal p-8 my-8 lg:p-16 lg:my-20 lg:text-xl">
             <p class="text-white text-center"><strong>Nuestro objetivo es brindarte soluciones sostenibles para tus
                     necesidades diarias, sin comprometer el confort ni la calidad. Creemos firmemente que cada pequeña
                     acción cuenta y que juntos podemos marcar la diferencia en la protección de nuestro entorno.</strong>
             </p>
         </div>
 
-        <div class="flex justify-between flex-wrap mb-20 text-center">
-            <div class="">
+        <div class="flex justify-center lg:justify-between flex-wrap mb-20 text-center">
+            <div class="my-3">
                 <div>
-                    <img class="rounded-full" src="../public/img/emp-3.webp" alt="">
+                    <img class="rounded-full" src="../public/img/emp-3.webp" alt="foto de Ernesto Arriaga, integrante del staff de la empresa">
                 </div>
                 <div>
                     <p class="text-xl font-semibold mt-4">Ernesto Arriaga</p>
                     <p>Presidente</p>
                 </div>
             </div>
-            <div class="">
+            <div class="my-3">
                 <div>
-                    <img class="rounded-full" src="../public/img/emp-2.webp" alt="">
+                    <img class="rounded-full" src="../public/img/emp-2.webp" alt="foto de Micaela Arriaga, integrante del staff de la empresa">
                 </div>
                 <div>
                     <p class="text-xl font-semibold mt-4">Micaela Arriaga</p>
                     <p>Responsable de Ventas</p>
                 </div>
             </div>
-            <div class="">
+            <div class="my-3">
                 <div>
-                    <img class="rounded-full" src="../public/img/emp-4.webp" alt="">
+                    <img class="rounded-full" src="../public/img/emp-4.webp" alt="foto de Federico Risso, integrante del staff de la empresa">
                 </div>
                 <div>
                     <p class="text-xl font-semibold mt-4">Federico Risso</p>
                     <p>Diseño de Producto</p>
                 </div>
             </div>
-            <div class="">
+            <div class="my-3">
                 <div>
-                    <img class="rounded-full" src="../public/img/emp-1.webp" alt="">
+                    <img class="rounded-full" src="../public/img/emp-1.webp" alt="foto de >Andres Barcala, integrante del staff de la empresa">
                 </div>
                 <div>
                     <p class="text-xl font-semibold mt-4">Andres Barcala</p>
