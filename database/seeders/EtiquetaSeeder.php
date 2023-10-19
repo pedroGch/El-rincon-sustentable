d066c6e66a8ac58c5fcc85cf7fa22f09e2e761fd
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-19 11:41:54 -0300

    cargo informacion en seeder etquita

--- database/seeders/EtiquetaSeeder.php
+++ database/seeders/EtiquetaSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class EtiquetaSeeder extends Seeder
 {
@@ -12,6 +13,31 @@ class EtiquetaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+      DB::table('etiquetas')->insert(
+        [
+          'etiqueta_id' => 1,
+          'nombre' => 'en promoción',
+          'created_at' => now(),
+          'updated_at' => now(),
+        ],
+        [
+          'etiqueta_id' => 2,
+          'nombre' => 'sin stock',
+          'created_at' => now(),
+          'updated_at' => now(),
+        ],
+        [
+          'etiqueta_id' => 3,
+          'nombre' => 'importado',
+          'created_at' => now(),
+          'updated_at' => now(),
+        ],
+        [
+          'etiqueta_id' => 4,
+          'nombre' => 'novedad',
+          'created_at' => now(),
+          'updated_at' => now(),
+        ],
+      );
     }
 }
