90ce112fdd7e037b5bd239fb9d8cebb0e1b1b83c
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-19 11:34:34 -0300

    creo el modelo, migracionde y seeder de etiquetas

---
+++ database/migrations/2023_10_19_140323_create_etiquetas_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('etiquetas', function (Blueprint $table) {
+            $table->tinyIncrements('etiqueta_id');
+            $table->string('nombre', 30);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('etiquetas');
+    }
+};
