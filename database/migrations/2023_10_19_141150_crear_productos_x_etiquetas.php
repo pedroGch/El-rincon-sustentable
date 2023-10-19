352d27f0c01881790727e4cbc24bbfd6931fe9e4
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-19 11:34:57 -0300

    creo la migracion de productos por etiquetas

---
+++ database/migrations/2023_10_19_141150_crear_productos_x_etiquetas.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+      Schema::create('productos_x_etquitas', function (Blueprint $table) {
+        //creamos las referencias a la tabla etiquetas
+        $table->unsignedTinyInteger('etiqueta_id');
+        $table->foreign('etiqueta_id')->references('tequita_id')->on('etiquetas');
+
+        //creamos las referencias (de la forma corta) a la tabla productos
+        $table->foreignId('id')->constrained('productos', 'id');
+
+        //aclaramos que la clave primaria es la combinacion de las claves primarias de producto y etiqueta
+        $table->primary(['id', 'etiqueta_id']);
+
+        $table->timestamps();
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+      Schema::dropIfExists('productos_x_etquitas');
+    }
+};
