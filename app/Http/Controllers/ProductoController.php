1cb745edc015609a7bffbad970bb1d6857b2f7b1
Pedro Gonzalez <pedro.gch01@gmail.com>
2023-10-19 17:39:38 -0300

    agrego inconpleto la accion de alta de producto

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -15,7 +15,7 @@ class ProductoController extends Controller
   public function index()
   {
     // $productos = Producto::all();
-    $productos = Producto::with('categoria')->get();
+    $productos = Producto::with('categoria', 'etiquetas')->get();
 
     return view('tienda', ["productos" => $productos]);
   }
@@ -27,5 +27,21 @@ class ProductoController extends Controller
   public function detalleProducto(int $id)
   {
     return view('detalle_producto', ["producto" => Producto::findOrFail($id)]);
+  }
+
+  /**
+   * Retorna la vista de la página del detalle de un producto
+   * @return \Illuminate\View\View
+   */
+  public function altaDeProducto(Request $request)
+  {
+    $data = 'algo que todavia no se que es';
+
+    $producto = Producto::create($data);
+    //aca decimos que agregamos la relacion de etiquitas, si es que vino en el request
+    //de lo contrario mandamos un array vacio
+    $producto->etiquetas()->attach($request->input('etiquetas') ?? []);
+
+    return view('detalle_producto', ["producto" => $producto]);
   }
 }
