ccc22aaaf27a51736dd46fc5e4b3a4953418e435
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-11-04 11:31:36 -0300

    fix tailwind importacion

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 import './bootstrap';
+import {
+    Carousel,
+    Collapse,
+    Dropdown,
+    Select,
+    initTE,
+  } from "tw-elements";
+
+  initTE({
+    Carousel,
+    Collapse,
+    Dropdown,
+    Select
+});
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,11 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900&display=swap"
+      rel="stylesheet" />
     @vite('resources/css/styles.css')
+    @vite('resources/js/app.js')
 
     <title> @yield('title') :: El Rincón Sustentable</title>
 </head>
@@ -343,21 +347,3 @@ data-te-navbar-ref>
 </body>
 
 </html>
-
-<script>
-  // Initialization for ES Users
-  import {
-    Carousel,
-    Collapse,
-    Dropdown,
-    Select,
-    initTE,
-  } from "tw-elements";
-
-  initTE({
-    Carousel,
-    Collapse,
-    Dropdown,
-    Select
-  });
-</script>
