eebf134d35ab567bc04351a6e7db760393cc9961
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-12-19 08:26:21 -0300

    indentación tabla noticias

--- resources/views/blog/tabla_noticias.blade.php
+++ resources/views/blog/tabla_noticias.blade.php
@@ -5,88 +5,74 @@
 ?>
 @extends('layouts.main')
 
-{{-- @section('title') Blog @endsection --}}
-
 @section('title', 'Gestor de Noticias')
 
 @section('content')
 
-
-    <section class="mx-auto max-w-screen-xl">
-        <div class="mb-5 flex justify-center">
-            <h2 class="text-principal my-4 mt-10 text-xl md:text-4xl font-semibold">Gestor de noticias</h2>
-
-            <div class="ps-10 mt-6 flex items-center">
-                <a href="<?= url('/blog/noticia/nueva') ?>">
-                    <div
-                        class="botonPersonalizado">
-                        <p>Crear noticia</p>
-                    </div>
-                </a>
-            </div>
+<section class="mx-auto max-w-screen-xl">
+  <div class="mb-5 flex justify-center">
+    <h2 class="text-principal my-4 mt-10 text-xl md:text-4xl font-semibold">Gestor de noticias</h2>
+    <div class="ps-10 mt-6 flex items-center">
+      <a href="<?= url('/blog/noticia/nueva') ?>">
+        <div class="botonPersonalizado">
+          <p>Crear noticia</p>
         </div>
-        <div class="lg:mx-6 mb-8 mt-2 flex justify-center flex-row flex-wrap">
-            <table>
-                <thead class="border-2">
-                    <tr>
-                        <th class="p-3">ID</th>
-                        <th class="p-3">Título</th>
-                        <th class="p-3">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody class="border-2">
-                    @foreach ($noticias as $noticia)
-                        <tr class="border-2">
-                            <td class="p-3 border-2">{{ $noticia->id }}</td>
-                            <td class="text-sm p-3 border-2">{{ $noticia->titulo }}</td>
-                            <td class="p-3 border-2">
-                                <form action="{{ url('/blog/' . $noticia->id . '/editar') }}" method="GET">
-                                    <button type="submit"
-                                        class="w-full botonPersonalizado-text-xs">Editar</button>
-                                </form>
+      </a>
+    </div>
+  </div>
+  <div class="lg:mx-6 mb-8 mt-2 flex justify-center flex-row flex-wrap">
+    <table>
+      <thead class="border-2">
+        <tr>
+          <th class="p-3">ID</th>
+          <th class="p-3">Título</th>
+          <th class="p-3">Acciones</th>
+        </tr>
+      </thead>
+      <tbody class="border-2">
+      @foreach ($noticias as $noticia)
+        <tr class="border-2">
+          <td class="p-3 border-2">{{ $noticia->id }}</td>
+          <td class="text-sm p-3 border-2">{{ $noticia->titulo }}</td>
+          <td class="p-3 border-2">
+            <form action="{{ url('/blog/' . $noticia->id . '/editar') }}" method="GET">
+              <button type="submit" class="w-full botonPersonalizado-text-xs">Editar</button>
+            </form>
+            <button type="button" onclick="borrarNoticia({{ $noticia->id }})" class="w-full botonPersonalizado-text-xs-rojo">Eliminar</button>
+            <form action="{{ url('/blog/' . $noticia->id . '/leer_mas') }}" method="GET">
+              <button type="submit" class="w-full botonPersonalizado-text-xs">Leer más</button>
+            </form>
+          </td>
+        </tr>
+      @endforeach
+      </tbody>
+    </table>
+  </div>
+</section>
 
-                                <button type="button" onclick="borrarNoticia({{ $noticia->id }})"
-                                    class="w-full botonPersonalizado-text-xs-rojo">Eliminar</button>
-
-                                <form action="{{ url('/blog/' . $noticia->id . '/leer_mas') }}" method="GET">
-                                    <button type="submit"
-                                        class="w-full botonPersonalizado-text-xs">Leer
-                                        más</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-    </section>
-
-    <script>
-        function borrarNoticia(id) {
-
-            Swal.fire({
-                title: '¿Estás seguro que querés eliminar la noticia?',
-                //text: "\"{{ $noticia->titulo }}\"",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonColor: '#459646',
-                cancelButtonColor: '#d33',
-                confirmButtonText: '¡Sí, borrar!',
-                cancelButtonText: 'Mejor no'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    //fetch(`${id}/eliminar`)
-                    fetch(`${id}/eliminar`, {
-                        method: 'get',
-                        headers: {
-                            'X-Requested-With': 'XMLHttpRequest'
-                        },
-                    })
-                    location.reload();
-                }
-            })
-
-
-        }
-    </script>
+{{-- MODAL CONFIRMACIÓN DE ELIMINACIÓN --}}
+<script>
+  function borrarNoticia(id) {
+    Swal.fire({
+      title: '¿Estás seguro que querés eliminar la noticia?',
+      //text: "\"{{ $noticia->titulo }}\"",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#459646',
+      cancelButtonColor: '#d33',
+      confirmButtonText: '¡Sí, borrar!',
+      cancelButtonText: 'Mejor no'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`${id}/eliminar`, {
+          method: 'get',
+          headers: {
+              'X-Requested-With': 'XMLHttpRequest'
+          },
+        })
+        location.reload();
+      }
+    })
+  }
+</script>
 @endsection
