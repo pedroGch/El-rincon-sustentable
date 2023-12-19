7d32dde3b2cbb11ea1944425b8f48015018d7dc2
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-12-18 22:53:56 -0300

    indentación en detalle noticia

--- resources/views/blog/detalle.blade.php
+++ resources/views/blog/detalle.blade.php
@@ -7,53 +7,48 @@
 
 @extends('layouts.main')
 
-{{-- @section('title') Blog @endsection --}}
-
 @section('title', 'Inicio')
 
 @section('content')
 
-
-    <section class="mx-auto max-w-screen-xl">
-        <div class="flex justify-center">
-            <h2 class="text-principal my-4 mt-10 text-xl md:text-4xl font-semibold">{{ $noticia->titulo }}</h2>
+<section class="mx-auto max-w-screen-xl">
+  <div class="flex justify-center">
+    <h2 class="text-principal my-4 mt-10 text-xl md:text-4xl font-semibold">{{ $noticia->titulo }}</h2>
+  </div>
+  <article class="lg:mx-6 lg:grid lg:grid-cols-2 w-full lg:p-4">
+    <div class="card-header m-4">
+      @if ($noticia->imagen !== null)
+          <img src="{{ asset('./storage/' . $noticia->imagen) }}" alt="{{ $noticia->alt }}">
+      @else
+          <p>Esta noticia no tiene imagen.</p>
+      @endif
+      <span class="fecha text-s my-1 text-gray-400">abril 29, 2023</span>
+    </div>
+    <div class="card-body m-4">
+      <p>{!! nl2br($noticia->contenido) !!}</p>
+    </div>
+  </article>
+</section>
+<section>
+  <h2 class="text-principal my-4 mt-10 text-xl md:text-4xl font-semibold">Seguir leyendo</h2>
+  <div class="lg:flex mb-8">
+    @foreach ($noticias as $cadaNoticia)
+      <div class="card m-5">
+        <a href="{{ url('/blog/' . $cadaNoticia->id . '/leer_mas') }}">
+          <div>
+            @if ($cadaNoticia->imagen !== null)
+              <img src="{{ asset('./storage/' . $cadaNoticia->imagen) }}" alt="{{ $cadaNoticia->alt }}" class="object-cover	w-90 h-50">
+            @else
+                <p>Esta noticia no tiene imagen.</p>
+            @endif
+          </div>
+        </a>
+        <div class="card-title my-1">
+          <h3 class="title font-bold text-principal">{{ $cadaNoticia->titulo }}</h3>
+          <span class="fecha text-xs text-gray-400">abril 29, 2023</span>
         </div>
-        <article class="lg:mx-6 lg:grid lg:grid-cols-2 w-full lg:p-4">
-            <div class="card-header m-4">
-                @if ($noticia->imagen !== null)
-                    <img src="{{ asset('./storage/' . $noticia->imagen) }}" alt="{{ $noticia->alt }}">
-                @else
-                    <p>Esta noticia no tiene imagen</p>
-                @endif
-
-                <span class="fecha text-s my-1 text-gray-400">abril 29, 2023</span>
-            </div>
-            <div class="card-body m-4">
-                <p>{!! nl2br($noticia->contenido) !!}</p>
-            </div>
-        </article>
-    </section>
-    <section>
-        <h2 class="text-principal my-4 mt-10 text-xl md:text-4xl font-semibold">Seguir leyendo</h2>
-        <div class="lg:flex mb-8">
-            @foreach ($noticias as $cadaNoticia)
-                <div class="card m-5">
-                    <a href="{{ url('/blog/' . $cadaNoticia->id . '/leer_mas') }}">
-                        <div>
-                            @if ($cadaNoticia->imagen !== null)
-                                <img src="{{ asset('./storage/' . $cadaNoticia->imagen) }}" alt="{{ $cadaNoticia->alt }}"
-                                    class="object-cover	w-90 h-50">
-                            @else
-                                <p>Esta noticia no tiene imagen</p>
-                            @endif
-                        </div>
-                    </a>
-                    <div class="card-title my-1">
-                        <h3 class="title font-bold text-principal">{{ $cadaNoticia->titulo }}</h3>
-                        <span class="fecha text-xs text-gray-400">abril 29, 2023</span>
-                    </div>
-                </div>
-            @endforeach
-        </div>
-    </section>
+      </div>
+    @endforeach
+  </div>
+</section>
 @endsection
