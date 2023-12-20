5115ed5b41566ac39661abfec8bfa1cfc8b4d48b
Rocio Scotto <104694565+roscotto@users.noreply.github.com>
2023-12-20 16:22:10 -0300

    datos facturación en checkout

--- resources/views/tienda/checkout.blade.php
+++ resources/views/tienda/checkout.blade.php
@@ -20,38 +20,46 @@ use MercadoPago\Resources\Preference;
 <section class="mx-auto max-w-screen-xl">
   <div class="row container mx-auto mt-4">
     <h2 class="text-principal my-4 mt-10 text-4xl font-semibold text-center">Checkout</h2>
-    <h3 class="my-8 text-lg">Resumen de tu compra:</h3>
-    @if(count($productos) > 0)
     <div>
-      <table class="table">
-        <thead>
-          <tr>
-            <th>Imagen</th>
-              <th>Nombre</th>
-              <th>Precio</th>
-              <th>Subtotal</th>
-          </tr>
-        </thead>
-        <tbody>
-        @foreach ($productos as $producto)
-          <tr class="border">
-            <td class="align-middle" width="25%">
-              <a href="{{ url('/producto/' . $producto->productos->id) }}">
-                <div class="flex justify-center">
-                  <img src="{{ asset('./storage/' . $producto->productos->imagen_prod) }}" alt="{{ $producto->productos->alt }}" class="img-fluid shadow-sm" width="100">
-                </div>
-              </a>
-            </td>
-            <td class="align-middle text-center" width="25%">{{ $producto->productos->nombre_prod }} ({{ $producto->cantidad_prod }} unidades)</td>
-            <td class="align-middle text-center" width="25%">${{ $producto->productos->precio }}</td>
-            <td class="align-middle text-center">${{ $producto->subtotal }}</td>
-          </tr>
-        @endforeach
-          <tr>
-            <td colspan="5" class="pt-6 align-middle text-center text-lg font-semibold" width="25%">Total $ {{ $totalPrice }}</td>
-          </tr>
-      </table>
+      <h3 class="my-8 text-lg font-bold">Tus datos para la facturación:</h3>
+      <p><b>Nombre:</b> <?= Auth::user()->name ?></p>
+      <p><b>Apellido:</b> <?= Auth::user()->surname ?> </p>
+      <p><b>Email:</b> <?= Auth::user()->email ?></p>
     </div>
+  </div>
+  <h3 class="my-8 text-lg font-bold">Resumen de tu compra:</h3>
+  @if(count($productos) > 0)
+  <div>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Imagen</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Subtotal</th>
+        </tr>
+      </thead>
+      <tbody>
+      @foreach ($productos as $producto)
+        <tr class="border">
+          <td class="align-middle" width="25%">
+            <a href="{{ url('/producto/' . $producto->productos->id) }}">
+              <div class="flex justify-center">
+                <img src="{{ asset('./storage/' . $producto->productos->imagen_prod) }}" alt="{{ $producto->productos->alt }}" class="img-fluid shadow-sm" width="100">
+              </div>
+            </a>
+          </td>
+          <td class="align-middle text-center" width="25%">{{ $producto->productos->nombre_prod }} ({{ $producto->cantidad_prod }} unidades)</td>
+          <td class="align-middle text-center" width="25%">${{ $producto->productos->precio }}</td>
+          <td class="align-middle text-center">${{ $producto->subtotal }}</td>
+        </tr>
+      @endforeach
+        <tr>
+          <td colspan="5" class="pt-6 align-middle text-center text-lg font-semibold" width="25%">Total $ {{ $totalPrice }}</td>
+        </tr>
+    </table>
+  </div>
+  <div>
     <!-- BOTÓN MERCADO PAGO -->
     <div id="checkout">
     </div>
